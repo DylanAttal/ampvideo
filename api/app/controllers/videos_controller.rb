a78eb8f96cce23addcd192d5a77443bdc35934b3
Dylan Attal <attal.dylan@gmail.com>
2022-01-24 18:57:30 -0500

    can see each matched video title and thumbnail. working on calculating video rank

--- api/app/controllers/videos_controller.rb
+++ api/app/controllers/videos_controller.rb
@@ -4,6 +4,18 @@ class VideosController < ApplicationController
 
     @matching_videos = Video.select { |video| video.title.include?(search_term) }.first(10)
 
-    render json: @matching_videos
+    @number_of_times_videos_viewed = VideoViewedLog.group(:video_id).count
+    puts "number of times each video was viewed hash"
+    puts @number_of_times_videos_viewed
+    # ((video_viewed_log.estimated_seconds_watched) / (video.video_length_second)) * (number of views)
+
+    results = @matching_videos.map { |video|
+      {
+        title: video.title,
+        thumbnail: video.thumbnailurl
+      }
+    }
+
+    render json: results
   end
 end
