0ed0f0db7e8c431937e5f96f8fa258f981830be8
Dylan Attal <attal.dylan@gmail.com>
2022-01-24 19:09:47 -0500

    calculates rank.. pretty sure the instructions dont use full formula...

--- api/app/controllers/videos_controller.rb
+++ api/app/controllers/videos_controller.rb
@@ -5,19 +5,24 @@ class VideosController < ApplicationController
     @matching_videos = Video.select { |video| video.title.include?(search_term) }.first(10)
 
     @number_of_times_videos_viewed = VideoViewedLog.group(:video_id).count
-    puts "number of times each video was viewed hash"
-    puts @number_of_times_videos_viewed
-    # ((video_viewed_log.estimated_seconds_watched) / (video.video_length_second)) * (number of views)
 
     videos_dto = @matching_videos.map { |video|
+      # ((video_viewed_log.estimated_seconds_watched) / (video.video_length_second)) * (number of views)
+
+      seconds_watched = VideoViewedLog.where(video_id: video.id).sum(:estimated_seconds_watched)
+      video_length = video.video_length_second
+      number_of_views = @number_of_times_videos_viewed[video.id]
+
+      video_rank = (seconds_watched / video_length) * number_of_views
 
       owner_name = VideoOwner.find{ |video_owner| video_owner.id == video.video_owner_id }.name
-      puts "owner name #{owner_name}"
 
       {
         title: video.title,
         thumbnail: video.thumbnailurl,
-        owner: owner_name
+        owner: owner_name,
+        views: number_of_views,
+        rank: video_rank
       }
     }
 
